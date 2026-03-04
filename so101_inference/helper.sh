1e4c1f92afa67acdb1783a4f0e48067ee0a2a1ab
Dmitri Manajev <dmitri@manajev.com>
2026-03-04 02:28:54 +0200

    undo

--- so101_inference/helper.sh
+++
@@ -1,7 +0,0 @@
-cd ~/ros2_ws/ && colcon build && source install/setup.bash && cd ~/ros2_ws/src/so101-ros-physical-ai
-
-pixi run -e lerobot infer
-pixi run -e lerobot async_infer -- --ros-args -p server_address:=145.236.164.238:24410 -p fps:=50.0 \
-    -p chunk_size_threshold:=0.5 -p aggregate_fn_name:=weighted_average
-
-
